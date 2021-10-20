f99ddb18d3e0afe37859e81b3ed1a21d4fde6757
Thanh <tnansci@gmail.com>
2021-10-20 14:34:26 -0400

    Initial commit

---
+++ src/com/company/Email.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Email {
+    private String fname;
+    private String lname;
+    private String pw;
+    private String dep;
+    private double mailBoxCap;
+    private String alEmail;
+}
