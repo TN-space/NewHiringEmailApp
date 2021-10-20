e3e40da9615c6fe9ebceb9f288abde3bbd93fedc
Thanh <tnansci@gmail.com>
2021-10-20 14:47:36 -0400

    Added sub steps + constructor

--- src/com/company/Email.java
+++ src/com/company/Email.java
@@ -1,10 +1,23 @@
 package com.company;
 
 public class Email {
-    private String fname;
-    private String lname;
+    private String firstName;
+    private String lastName;
     private String pw;
     private String dep;
     private double mailBoxCap;
     private String alEmail;
+
+    // Constructor to receive first and last name
+    public Email(String fName, String lName) {
+        this.firstName = fName;
+        this.lastName = lName;
+    }
+    // Ask user input for department
+
+    // Generate random password
+
+    // Set mailbox cap
+
+    // Change password
 }
